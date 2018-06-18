c3223c1f8ce8a5d05d56b024cf5b56ce448710c3
Raul <raulrodriguezmendez97@gmail.com>
2018-06-18 11:29:33 +0200

    Add files via upload

--- Estructura de datos.py
+++ Estructura de datos.py
@@ -165,6 +165,26 @@ def resultado_circuito_defectuoso (vector_entrada1, vector_entrada2, puertas_def
 
 print('Estructura de datos para la definición de un circuito')
 
+#CON UNA NOT AL FINAL EL METODO NO FUNCIONA
+#programacionPuertas = ['AND', 'OR', '--', 'OR', 'NOT', '--', 'NAND', 'AND', '--', 'XOR', 'AND', 'OR']
+#conexiones = [[0,1], 
+#          [1,2],
+#          [],
+#          [2,3],
+#          [[0,0]],
+#          [],
+#          [[0,0], [0,1]],
+#          [[0,1],[0,3]],
+#          [],
+#          [[0,0], [1,0]],
+#          [[1,0],[1,2]],
+#          [[1,2],[1,3]]]
+
+
+#m = 3
+#n = 4
+
+
 programacionPuertas = ['AND', 'OR', '--', 'NOT', '--', 'NAND', '--', 'XOR', 'AND']
 conexiones = [[0,1], 
           [1,2],
@@ -174,27 +194,88 @@ conexiones = [[0,1],
           [[0,0], [0,1]],
           [],
           [[0,0], [1,0]],
-          [[1,0], [1,2]]]
+          [[1,0],[1,2]]]
 
 
 m = 3
 n = 3
 
+
 circuito = estructura_circuito(m,n, programacionPuertas, conexiones)
 
 vector_entrada1 = [0,0,0]
-vector_entrada2 = [0,0,1]
+#vector_entrada2 = [0,0,1]
 puertas_defectuosas = [[0,1],
                        [1,2]]
 
-vector_salida = resultado_circuito (vector_entrada1, vector_entrada2, circuito, m, n)
-vector_salida_defectuoso = resultado_circuito_defectuoso(vector_entrada1, vector_entrada2, puertas_defectuosas, circuito, m, n)
+#vector_salida = resultado_circuito (vector_entrada1, vector_entrada2, circuito, m, n)
+#vector_salida_defectuoso = resultado_circuito_defectuoso(vector_entrada1, vector_entrada2, puertas_defectuosas, circuito, m, n)
 
 
 
 
-print("Vector de salida: ")
-print(vector_salida)
-print("Vector de salida defectuoso: ")
-print(vector_salida_defectuoso)
+#sprint("Vector de salida: ")
+#print(vector_salida)
+#print("Vector de salida defectuoso: ")
+#print(vector_salida_defectuoso)
+
+
+
+
+
+vectores_entrada_inicial= [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+
+def f_vectores_salida(vectores_entrada,vector_entrada1,circuito,m,n):
+    vec_salida=[]
+    vectores_entrada_nuevo= calcula_vectores_entrada(vectores_entrada,n)
+    for i in range(len(vectores_entrada_nuevo)):
+        vec_salida.append(resultado_circuito(vector_entrada1,vectores_entrada_nuevo[i],circuito,m,n))
+    return vec_salida
+
+#para n mayor que tres, cada vector de entrada se le añadirá un cero, es decir, si n fuese 5, el primer vector de entrada
+#seria [0,0,0,0,0] y el segundo seria [0,0,1,0,0]. Se le añade tantos ceros según la diferencia de n-3
+def calcula_vectores_entrada(vectores_entrada,n):
+    vectores_entrada_nuevo= vectores_entrada
+
+    if(n<3):
+        for i in range(len(vectores_entrada_nuevo)):
+            vectores_entrada_nuevo.pop()
+        vectores_entrada_nuevo.append([0,0])
+        vectores_entrada_nuevo.append([0,1])
+        vectores_entrada_nuevo.append([1,0])
+        vectores_entrada_nuevo.append([1,1])
+    elif (n>3):
+        vectores_entrada_nuevo=vectores_entrada
+        for i in range(len(vectores_entrada)):
+            for j in range(3,n):
+                vectores_entrada_nuevo[i].append(0)
+    return vectores_entrada_nuevo
+
+
+def auto_diagnostico(vect_ent):
+    res= True
+    vectores_e=calcula_vectores_entrada(vectores_entrada_inicial,n)#Vectores entrada
+    vectores_s= f_vectores_salida(vectores_entrada_inicial,vector_entrada1,circuito,m,n)#Vectores salida
+
+    for i in range(len(vectores_e)):
+        for j in range(len(vectores_e[i])):
+            if(vectores_e[i][j]==vect_ent[j]):
+                vect_sal_aux= resultado_circuito(vector_entrada1,vect_ent,circuito,m,n)
+
+                for z in range(len(vectores_s)):
+                    for x in range(len(vectores_s[z])):
+                        if(vectores_s[z][x] != vect_sal_aux[x]):
+                            res=False
+                            break
+                        break
+                    break
+                break
+            break
+        break
+    return res
+
+print(calcula_vectores_entrada(vectores_entrada_inicial,n))
+print(f_vectores_salida(vectores_entrada_inicial,vector_entrada1,circuito,m,n))
+print(auto_diagnostico([1,1,0]))
+
 
